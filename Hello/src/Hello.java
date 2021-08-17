b263ef342f1496820fdf45c652e32c904206e9b1
regex <cosibekelly@gmail.com>
2021-08-17 15:44:14 +0100

    Chapter one, exercise 1 completed.

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); // Added the hello world program
+        System.out.println("Are you alright?"); // Added the second task
     }
 }

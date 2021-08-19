a80cd9fc3b2caab701bf1eda854eb037cb296627
regex <cosibekelly@gmail.com>
2021-08-19 16:09:00 +0100

    Chapter two, exercise 1

---
+++ chapterTwo/src/Date.java
@@ -0,0 +1,22 @@
+public class Date {
+    public static void main(String[] args){
+//        question 1
+        System.out.println("Hello World! \n");
+
+//        question 2
+        String day = "Thursday";
+        int date = 19;
+        String month = "August";
+        int year = 2021;
+
+//        question 3
+        System.out.println(day + "\n" + date + "\n" + month + "\n" + year + "\n");
+
+//        question 4
+        System.out.println(day + ", " + month + ", " + date + ", " + year + "\n");
+
+//        question 5
+        System.out.println("American Format: " + day + ", " + month + ", " + date + ", " + year);
+        System.out.println("European Format: " + day + ", " + date + ", " + month + ", " + year);
+    }
+}

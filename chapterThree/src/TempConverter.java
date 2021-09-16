7fdec7f867b09bcb55fcbafd2eed53cb207b0b87
regex <cosibekelly@gmail.com>
2021-09-16 23:51:20 +0100

    modified chapterThree

--- chapterThree/src/TempConverter.java
+++ chapterThree/src/TempConverter.java
@@ -4,9 +4,9 @@ public class TempConverter {
     public static void main(String[] args){
         System.out.print("Enter a temperature in Celsius: ");
         Scanner in = new Scanner(System.in);
-        double x = in.nextDouble();
-        final double m = (double) (9 / 5) + 32;
-        double F = x * m;
-        System.out.printf("%.2fC = %.2fF", x, F);
+        double C = in.nextDouble();
+        final double m = (9.0 / 5.0) + 32.0;
+        double F = C * m;
+        System.out.printf("%.2fC = %.2fF", C, F);
     }
 }
